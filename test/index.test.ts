dd451b7a4caf10d5b1288f88f8c91ba3ebc50162
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-22 01:00:29 +0200

    Migrate to .ts

---
+++ test/index.test.ts
@@ -0,0 +1,52 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+import { readFileSync } from 'node:fs'
+import { assert, describe, suite, test } from 'vitest'
+import { extractHeaderFromFile, extractHeaderString } from '../src/index.js'
+
+const advoid: string = readFileSync('./test/data/result/header.txt', 'utf-8')
+
+describe('🧪 adblock header extract tests 🧪', () => {
+  suite('extractHeaderString()', () => {
+    test('#1 should return an empty string', () => {
+      // @ts-expect-error
+      assert.isEmpty(extractHeaderString())
+    }) // #1
+
+    test('#2 should return an empty string', () => {
+      assert.isEmpty(extractHeaderString(''))
+    }) // #2
+  })
+
+  suite('extractHeaderFromFile()', () => {
+    test('#3 should throw', () => {
+      assert.throws(() => {
+        // @ts-expect-error
+        extractHeaderFromFile()
+      })
+    }) // #3
+
+    test('#4 should return an empty string', () => {
+      assert.throws(() => {
+        extractHeaderFromFile('fake.txt')
+      })
+    }) // #4
+
+    test('#5 should return the file header (trim)', () => {
+      const result: string = extractHeaderFromFile(
+        './test/data/filter/advoid.txt'
+      )
+
+      assert.equal(result, advoid)
+    }) // #5
+
+    test('#6 should return the file header (no trim)', () => {
+      const result: string = extractHeaderFromFile(
+        './test/data/filter/advoid.txt',
+        false
+      )
+
+      assert.equal(result, `${advoid}\r\n`)
+    }) // #6
+  })
+})
